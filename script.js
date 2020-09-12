04c7d77519f7928843eb86b97d19c5e37f7a30cf
Insomnia <jamie.mar.jones14@gmail.com>
2020-09-12 14:34:02 -0400

    Local Storage works now

--- script.js
+++ script.js
@@ -9,48 +9,91 @@ console.log("Lexa deserved better");
 var currentDay = $("#currentDay");
 var calenderEl = $(".container");
 var now = moment().format("dddd, MMMM Do, YYYY");
-var daysTimes = moment().format("LT");
+var daysTime = moment().format("LT");
+var past = $(".past");
+var present = $(".present");
+var future = $(".future");
+// var m = moment();
 
-var timeArray = [
-  moment("9:00 AM", "h A"),
-  moment("10:00 AM", "h A"),
-  moment("11:00 AM", "h A"),
-  moment("12:00 PM", "h A"),
-  moment("1:00 PM", "h A"),
-  moment("2:00 PM", "h A"),
-  moment("3:00 PM", "h A"),
-  moment("4:00 PM", "h A"),
-  moment("5:00 PM", "h A"),
-];
-console.log(timeArray[0]._i);
+console.log(daysTime);
+// console.log(m);
+
+// var timeArray = [
+//   moment("9:00 AM", "h A"),
+//   moment("10:00 AM", "h A"),
+//   moment("11:00 AM", "h A"),
+//   moment("12:00 PM", "h A"),
+//   moment("1:00 PM", "h A"),
+//   moment("2:00 PM", "h A"),
+//   moment("3:00 PM", "h A"),
+//   moment("4:00 PM", "h A"),
+//   moment("5:00 PM", "h A"),
+// ];
+// console.log(timeArray[0]._i);
+
+var hoursArray = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
 
 $(document).ready(function () {
+  // DATE APPENDED TO TOP OF PAGE
   $("#currentDay").append(now);
 
   // DYNAMICALLY GENERATING
-  for (var j = 0; j < timeArray.length; j++) {
+  for (var i = 0; i < hoursArray.length; i++) {
     var rowEl = $("<row>");
     rowEl.addClass("row time-block");
-    // rowEl.text("am I here");
     calenderEl.append(rowEl);
 
     var hourEl = $("<div>");
     hourEl.addClass("time-block col-sm-2 hour");
-    hourEl.text(timeArray[j]._i);
+    hourEl.text(hoursArray[i] + ":00");
     rowEl.append(hourEl);
 
     var textEl = $("<textarea>");
-    textEl.addClass("textarea col-sm-8");
+    textEl.addClass("textarea col-sm-8 description");
     textEl.attr("placeholder, enter your todo list");
+    textEl.attr("data-hour", hoursArray[i]);
+    if (localStorage.getItem(hoursArray[i]) != null) {
+      textEl.text(localStorage.getItem(hoursArray[i]));
+    }
+    // if (hoursArray[i] < daysTime) {
+    //   textEl.addClass("past");
+    // } else if (hoursEl === daysTime) {
+    //   textEl.addClass("present");
+    // } else {
+    //   textEl.addClass("future");
+    // }
     rowEl.append(textEl);
 
     var buttonEl = $("<button>");
     buttonEl.addClass("saveBtn col-sm-2");
     buttonEl.text("SAVE");
+    buttonEl.attr("data-hour", hoursArray[i]);
     rowEl.append(buttonEl);
   }
 
-  var plannedEvents = localStorage.setItem(textEl).value;
+  // LOCAL STORAGE
+  $(".saveBtn").on("click", function () {
+    var saveHour = $(this).attr("data-hour");
+    console.log(saveHour);
+
+    var saveText = $(this).siblings(".description").val();
+    console.log(saveText);
+
+    localStorage.setItem(saveHour, saveText);
+  });
+
+  // CONNECT ACTIVE CURRENT TIME TO THE HOUREL
+  // setInterval
+
+  // CONDITIONAL FOR THE ROW COLORS
+  // if (hourEl < currentHour) {
+  //   rowEl.addClass("past");
+  // } else if (hourEl === currentHour) {
+  //   rowEl.addClass("present");
+  // } else {
+  //   rowEl.addClass("future");
+  // }
+  // console.log(currentHour);
 });
 
 //   // $(".btn").on("click", function () {
